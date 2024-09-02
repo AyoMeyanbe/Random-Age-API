4a0467064520cc968d7d9d57a11cdf1e0c926a89
AyoMeyanbe <ayomeyanbe@gmail.com>
2024-09-01 23:37:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios, { Axios } from "axios";
 
 function App() {
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
+  const fetchData = () => {
+    axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPredictedAge(res.data)
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="Ex. Ayo..."
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <button onClick={fetchData}>Predict Age</button>
+
+      <h1> Name: {predictedAge?.name}</h1>
+      <h1> Predicted Age: {predictedAge?.age}</h1>
+      <h1> Count: {predictedAge?.count}</h1> 
+      {/* '?' means access it if it is not null */}
     </div>
   );
 }
